aae334f747c33f0852e422a35eea47b6db27a7c0
mengrLi <34010243+mengrLi@users.noreply.github.com>
2018-03-10 16:48:42 -0500

    Update Solution.java

--- src/main/java/q7_power/Solution.java
+++ src/main/java/q7_power/Solution.java
@@ -1,7 +1,21 @@
 package q7_power;
 
-public class Solution {
-    public int power(int base, int power) {
+ public class Solution {
+    public int power(int base, int power)
+   {
+
+        int x = base;
+
+        int n = power;
+
+
+        return function(x, n);
+
+    }
+    public static int function (int x,int n){
+        if(n > 0){
+            return x*function(x, n - 1);
+        }
         return 1;
     }
 }
